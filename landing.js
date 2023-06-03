019dd74de2c341f5ef1c70ce37ed77dc003a4e47
Helena <helena@sixfeetcreative.com>
2023-06-03 15:32:47 +0200

    added countdown, mobile/desktop instructions

--- landing.js
+++ landing.js
@@ -1,12 +1,109 @@
+import { gsap } from "gsap";
+
+// Rest of your code here
+
 //playing the animation with values from audio
 const container = document.getElementById("landingContainer");
 
 function greeting() {
-  const div = document.createElement("div");
-  div.innerHTML = "Greetings, the game will start in 17 seconds";
-  div.id = "greeting";
+  const h1 = document.createElement("h1");
+  h1.textContent = "The Game starts in:";
+  h1.id = "landingTitle";
+  container.appendChild(h1);
+  setTimeout(displayCountdown);
+  setTimeout(displayInstructions, 200);
+}
 
-  container.appendChild(div);
+function displayInstructions() {
+  var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  if (isMobile) {
+    playingMobile();
+  } else {
+    playingDesktop();
+  }
+}
+
+function playingMobile() {
+  const instructions = document.createElement("p");
+  instructions.style.opacity = 0;
+  instructions.id = "landingInstructionsMobile";
+  container.appendChild(instructions);
+
+  const textStrings = [
+    "Hello and welcome to the Mobile Experience of the <br> Yung Titties Game",
+    "Click the balls when they are inside the circle.",
+    "Try to get the best accuracy possible!<br> Good luck brah.",
+  ];
+  let currentIndex = 0;
+
+  function displayNextString() {
+    gsap.to(instructions, {
+      opacity: 0,
+      duration: 0.5,
+      onComplete: updateText,
+    });
+  }
+
+  function updateText() {
+    instructions.innerHTML = textStrings[currentIndex];
+    gsap.to(instructions, { opacity: 1, duration: 0.5, delay: 0.5 });
+    currentIndex = (currentIndex + 1) % textStrings.length;
+  }
+
+  updateText();
+  setInterval(displayNextString, 5000);
+}
+
+function playingDesktop() {
+  const instructions = document.createElement("p");
+  instructions.style.opacity = 0;
+  instructions.id = "landingInstructionsDesktop";
+  container.appendChild(instructions);
+
+  const textStrings = [
+    "Hello and welcome to the Desktop Experience of the <br>Yung Titties Game",
+    "Use the keys 'A', 'S' and 'D' on your keyboard.<br> Try hitting the balls when they reach the circle.",
+    "Try to get the best accuracy possible! <br>Good luck brah.",
+  ];
+  let currentIndex = 0;
+
+  function displayNextString() {
+    gsap.to(instructions, {
+      opacity: 0,
+      duration: 0.5,
+      onComplete: updateText,
+    });
+  }
+
+  function updateText() {
+    instructions.innerHTML = textStrings[currentIndex];
+    gsap.to(instructions, { opacity: 1, duration: 0.5, delay: 0.5 });
+    currentIndex = (currentIndex + 1) % textStrings.length;
+  }
+
+  updateText();
+  setInterval(displayNextString, 5000);
+}
+
+function displayCountdown() {
+  var countdown = 15;
+
+  const countdownDisplay = document.createElement("p");
+  countdownDisplay.textContent = countdown;
+  container.appendChild(countdownDisplay);
+  countdownDisplay.id = "landingCountdown";
+
+  var countdownInterval = setInterval(function () {
+    countdown--;
+    if (countdown === 0) {
+      countdownDisplay.textContent = "GO!";
+      clearInterval(countdownInterval);
+      return;
+    }
+
+    countdownDisplay.textContent = countdown;
+  }, 1000);
 }
 
 function removeGreeting() {
@@ -14,11 +111,11 @@ function removeGreeting() {
 }
 
 export function landing() {
-  function runForFiveSeconds() {
+  function runForSixteenSeconds() {
     greeting();
     setTimeout(function () {
       removeGreeting();
     }, 16000); // 5000 milliseconds = 5 seconds
   }
-  runForFiveSeconds();
+  runForSixteenSeconds();
 }
