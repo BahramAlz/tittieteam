0c19c2b5d9a4521f1f798b580d295fb05cf92ac4
Bahram Al-Zuhairi <bahram.alzuhairi@hyperisland.se>
2023-07-03 12:51:47 +0200

    volume

--- score.js
+++ score.js
@@ -1,5 +1,3 @@
-let circularProgress = document.getElementById("circularProgress");
-
 let score = 0;
 const maxScore = 100;
 
@@ -7,7 +5,6 @@ export function addScore(scoreType) {
   if (scoreType === "hit") {
     if (score === 0) {
       score += 30;
-      circularProgress.style.background = `conic-gradient(#7d2ae8 ${score}deg, #ededed 0deg)`;
     } else if (score > 100) {
       score = Math.floor(score / 100) * 100;
     } else {
@@ -15,13 +12,13 @@ export function addScore(scoreType) {
     }
   } else if (scoreType === "close") {
     score *= 0.7;
-    circularProgress.style.background = `conic-gradient(#7d2ae8 ${score}deg, #ededed 0deg)`;
   }
 
   // Restrict score to be within the range of 0 to 100
   score = Math.max(0, Math.min(score, maxScore));
 
   updateScoreDisplay(score);
+  updateFill(score); // Call the updateFill function with the updated score
   console.log(score);
 }
 
@@ -29,17 +26,24 @@ export function decreaseScore() {
   score -= 5;
   score = Math.max(0, score); // Ensure score never goes below 0
   updateScoreDisplay(score);
+  updateFill(score); // Call the updateFill function with the updated score
 }
 
 function updateScoreDisplay(score) {
   const percentage = Math.floor((score / maxScore) * 100);
   const scoreDisplay = document.getElementById("scoreDisplay");
-
   scoreDisplay.textContent = `${percentage}%`;
+}
+
+function updateFill(score) {
+  const semiDonut = document.getElementById('semi-donut');
+  semiDonut.style.setProperty('--percentage', `${score}%`);
+  semiDonut.textContent = `${score}%`;
 }
 
 export function resetScore() {
   updateScoreDisplay(0);
+  updateFill(0); // Call the updateFill function to reset the fill to 0%
 }
 
 // export default score;
--- song.js
+++ song.js
@@ -4,25 +4,25 @@
 // sourceNode.connect(audioCtx.destination); // connecting audio directly to spekaers, we need to start
 let audioCtx;
 export function playSong() {
-	const audioEl = new Audio("song.mp3");
-	audioCtx = new AudioContext();
-	const sourceNode = audioCtx.createMediaElementSource(audioEl); //this is our source, moving it to the audio thread!
-	const gainNode = audioCtx.createGain();
-	sourceNode.connect(gainNode);
-	gainNode.connect(audioCtx.destination);
-	// ^^ connecting the gainnode, which makes it possible for us to manipulate the gain, ex:
-	gainNode.gain.value = 0.1; //setting the volume to 0.1 percent
+  const audioEl = new Audio("song.mp3");
+  audioCtx = new AudioContext();
+  const sourceNode = audioCtx.createMediaElementSource(audioEl); //this is our source, moving it to the audio thread!
+  const gainNode = audioCtx.createGain();
+  sourceNode.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
+  // ^^ connecting the gainnode, which makes it possible for us to manipulate the gain, ex:
+  gainNode.gain.value = 0.1; //setting the volume to 0.1 percent
 
-	const volumeBar = document.getElementById("volumeBar");
-    volumeBar.addEventListener("input", function () {
-      const volume = parseFloat(this.value);
-      gainNode.gain.value = volume;
-    });
+  // const volumeBar = document.getElementById("volumeBar");
+  // volumeBar.addEventListener("input", function () {
+  //   const volume = parseFloat(this.value);
+  //   gainNode.gain.value = volume;
+  // });
 
-	audioCtx.resume(); // the ctx isnt active by default, we need to resume it
-	audioEl.play();
+  audioCtx.resume(); // the ctx isnt active by default, we need to resume it
+  audioEl.play();
 }
 
 export function stopSong() {
-	audioCtx.close();
+  audioCtx.close();
 }
